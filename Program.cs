37876a26ed714b869e4bcbe14283f20dc667d07e
Admin <Admin@DESKTOP-89NTJEN>
2019-05-22 11:41:35 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asdmNumbers
+{
+    class Program
+    {
+        // program to print on screen the output of adding, subtracting,
+        //multiplying and dividing of 
+        //two numbers which will be entered by the user. 
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0} + {1} = {2}", num1, num2, num1 + num2);
+            Console.WriteLine("{0} - {1} = {2}", num1, num2, num1 - num2);
+            Console.WriteLine("{0} x {1} = {2}", num1, num2, num1 * num2);
+            Console.WriteLine("{0} / {1} = {2}", num1, num2, num1 / num2);
+            Console.WriteLine("{0} mod {1} = {2}", num1, num2, num1 % num2);
+        }
+    }
+    }
+
